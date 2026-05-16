b4cddb6877d49544f2ffd49e0a628e3602a73c81
fyoneyone9-cyber <f.yoneyone9@gmail.com>
2026-05-17 00:23:29 +0900

    fix: HTMLリンク出力対応(sanitize:false)

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -40,7 +40,7 @@ export async function getPostData(slug: string): Promise<PostData> {
   const fullPath = path.join(postsDirectory, `${slug}.md`)
   const matterResult = matter(fs.readFileSync(fullPath, 'utf8'))
   const contentHtml = (
-    await remark().use(html).process(matterResult.content)
+    await remark().use(html, { sanitize: false }).process(matterResult.content)
   ).toString()
   return {
     slug,
